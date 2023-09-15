e94d1c0bc3dcb610bafbb2361f4db1b20217cfbe
ThulaSrivarshini <133554544+ThulaSrivarshini@users.noreply.github.com>
2023-09-15 14:18:32 +0000

    vhjk

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
     return 'Hello world', 200
+    return ''
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
@@ -14,6 +15,7 @@ def add():
     second=data['second']
     result=first+second
     return jsonify({'result':result}), 200
+    return ''
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
@@ -21,6 +23,7 @@ def subtract():
     second=data['second']
     result=first-second
     return jsonify({'result':result}), 200
+    return ''
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
